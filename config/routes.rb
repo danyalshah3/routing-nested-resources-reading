fc2ca000d075f46fbb291f1cb51342b587f94d01
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-07-02 08:35:30 -0400

    commit tree b862eeccab7fffed615487e74c1bdd609ce3efe5

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+  resources :posts, only: [:show, :index]
+
+
   root 'posts#index'
 
 end
